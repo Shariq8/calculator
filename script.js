80a1700d6df925360fb895771048f360395618e7
Shariq <shariq7991@gmail.com>
2021-09-03 16:35:24 +0100

    basic operations

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-const add = function(a,b){
-    return a + b;
-}
+var input1, input2, operator, ans;
 
-const sub = function(a,b){
-    return a - b;
-}
+const add = function(a,b){return a + b;}
 
-const mul = function(a,b){
-    return a * b;
-}
+const sub = function(a,b){return a - b;}
 
-const div = function(a,b){
-    return a / b;
-}
+const mul = function(a,b){return a * b;}
 
-const pow = function(a){
-    return a*a;
-}
+const div = function(a,b){return a / b;}
+
+const pow = function(a){ return a*a;}
 
 const operate = function(op, a, b){
     switch(op){
@@ -32,26 +24,52 @@ const operate = function(op, a, b){
 }
 
 const btns = document.querySelectorAll('.btn');
-const input = document.querySelector('#input');
-const ouput = document.querySelector('#output');
+const topScr = document.querySelector('#input'); //Top part
+const botScr = document.querySelector('#output'); //Bottom part
 
 btns.forEach(button => button.addEventListener('click', function() {
-    input.innerHTML += button.innerHTML;
-}));
+    botScr.innerHTML += button.innerHTML;
+})); //Adds the value to the bottom screen
 
-const del = document.querySelector('#delete');
+const del = document.querySelector('#delete'); //Deletes the most recent number
 del.addEventListener('click', function(){
     var str = output.innerHTML;
-    output.innerHTML = str.slice(0,-1);
+    botScr.innerHTML = str.slice(0,-1);
 });
 
-const ac = document.querySelector('#clear');
+const ac = document.querySelector('#clear'); //Clears the text on screen
 ac.addEventListener('click', function(){
-    output.innerHTML = "";
+    botScr.innerHTML = topScr.innerHTML = "";
+    input1 = undefined;
 })
 
-const op = document.querySelectorAll('#op');
+const op = document.querySelectorAll('.op');
 op.forEach(operate => operate.addEventListener('click', function(){
-    const input = document.querySelector('#input');
-    output.innerHTML()
+    operations(botScr.innerHTML, operate.id, operate.innerHTML);
 }))
+
+function operations(val, opCode, opVal){ //Takes the number, the id value and the sign value for scr
+    if(input1 == undefined){ //Means there needs to be input1 and op (initial)
+        input1 = val;
+        operator = opCode;
+        topScr.innerHTML = input1 + ' ' + opVal;
+        botScr.innerHTML = '';
+    }else if(operator == undefined){
+        operator = opCode;
+        topScr.innerHTML = input1 + ' ' + opVal;
+        botScr.innerHTML = '';
+    }else if(input2 == undefined){ //Means there's an input1 but not input2 
+        input2 = val; //Calcuate
+        input1 = operate(operator, parseInt(input1),parseInt(input2)); //Makes input1 the new value
+        if(opVal == '='){
+            topScr.innerHTML += ' ' + input2 + ' =' 
+            botScr.innerHTML = input1;
+            input2 = operator = undefined;
+        }else{
+            topScr.innerHTML = input1 + ' ' + opVal;
+            botScr.innerHTML = '';
+            input2 = undefined;
+            operator = opCode;
+        }
+    }
+}
