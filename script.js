c0f078e44d4c4ae6b977d901dc837438a6e68eb7
Shariq <shariq7991@gmail.com>
2021-09-03 15:08:27 +0100

    basic ui

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ const div = function(a,b){
     return a / b;
 }
 
+const pow = function(a){
+    return a*a;
+}
+
 const operate = function(op, a, b){
     switch(op){
         case 'add':
@@ -26,3 +30,28 @@ const operate = function(op, a, b){
             return div(a,b);
     }
 }
+
+const btns = document.querySelectorAll('.btn');
+const input = document.querySelector('#input');
+const ouput = document.querySelector('#output');
+
+btns.forEach(button => button.addEventListener('click', function() {
+    input.innerHTML += button.innerHTML;
+}));
+
+const del = document.querySelector('#delete');
+del.addEventListener('click', function(){
+    var str = output.innerHTML;
+    output.innerHTML = str.slice(0,-1);
+});
+
+const ac = document.querySelector('#clear');
+ac.addEventListener('click', function(){
+    output.innerHTML = "";
+})
+
+const op = document.querySelectorAll('#op');
+op.forEach(operate => operate.addEventListener('click', function(){
+    const input = document.querySelector('#input');
+    output.innerHTML()
+}))
